22f538ebcf149c8000798cb42047a6db7c7e5e47
BbangGae <ydh1390@gmail.com>
2025-05-26 14:55:02 +0900

    플레이어 컨트롤러, 카메라추적, 애너미 추적 구현

---
+++ Assets/Scripts/ScriptableObject/Enemy/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy_Data", menuName = "ScriptableObject/Enemy", order = 3)]
+public class Enemy : ScriptableObject
+{
+    public Enemy()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ScriptableObject/Player/player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player_Data", menuName = "ScriptableObject/Player", order = 2)]
+public class player : ScriptableObject
+{
+ public player()
+    {
+
+    }
+}
