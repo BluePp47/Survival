c369d72d2e4820e0b917771217d65944dd3131b3
BbangGae <ydh1390@gmail.com>
2025-05-26 19:27:44 +0900

    최신화

--- Assets/Item/DropItemData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DropItemData", menuName = "Item/DropItem")]
-public class DropItemData : ScriptableObject
-{
-    public GameObject itemPrefab;  // 드랍할 아이템 프리팹
-    public int dropChance = 100;   // 드랍 확률 (%)
-}
