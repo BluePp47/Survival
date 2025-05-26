985db68b8cf62b3f59ac139a41cff5771f4f5cc7
jellujoa <jellujoa03@gmail.com>
2025-05-26 17:14:39 +0900

    fix building 중간 저장

--- Assets/Scripts/Building System/Building System.cs
+++ Assets/Scripts/Building System/Building System.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor.Search;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BuildingSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 설치에 사용할 카메라 
+    public Camera mainCamera;
+
+    // 들고있는 건설 아이템 
+    public BuildItem bluePrint;
+
+    // 마우스 고스트(프리뷰) 옵젝
+    private GameObject ghostObject;
+
+    private void Update()
+    {    
+        // bluePrint를 들고있을때만 
+        if(bluePrint != null)
+        {
+            ShowGhostPrefab();
+            if (Input.GetMouseButtonDown(0))
+            {
+                // 마우스 위치로 ray를 쏴
+                Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+                // Ray가 Collider에 닿았는지 확인
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    Vector3 buildPosition = hit.point;
+                    PlacePrefab(buildPosition);
+                }
+            }
+
+        }
+        else
+        {// 없으면 부스고 
+            if(ghostObject != null)
+            {
+                Destroy(ghostObject);
+                ghostObject = null;
+            }
+        }
+    }
+
+    void ShowGhostPrefab()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    void PlacePrefab(Vector3 position)
     {
-        
+        // 프리팹 해당 위치 생성
+        Instantiate(bluePrint.prefab,position, Quaternion.identity);
     }
 }
