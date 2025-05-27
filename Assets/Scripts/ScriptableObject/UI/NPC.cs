2f5a0c9ff32d643d2aaf16988f8115c8fb771354
피타쵸쵸 <djfb123@naver.com>
2025-05-27 15:25:23 +0900

    [feat] npc SC added
    [feat] npc SC added

--- Assets/Scripts/ScriptableObject/UI/NPC.cs
+++ Assets/Scripts/ScriptableObject/UI/NPC.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 
 [CreateAssetMenu(fileName = "NPC",menuName ="NPC/Dialog",order = 6)]
 public class NPC : ScriptableObject
 {
-    public DialogOptOutDecisionType dialog;
+    public string NPCName;
+    public string dialogueText;
+    public string npcName;
 
+    public string option1Buton;
+    public string option2Buton;
+
+    public TextMeshProUGUI option1Text;
+    public TextMeshProUGUI option2Text;
+}
+
+public enum RewardType
+{
+    Blueprint,
+    Resource
 }
