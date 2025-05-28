aa8cd80a48a7d4dd909e2696cebbef57ad8cc355
kwangminleee <nuewaz@naver.com>
2025-05-28 10:37:42 +0900

    Fix : AttackRayerMask
    자원 인식 안되는 오류 수정

--- Assets/Scripts/ScriptableObject/Item/ItemPickup.cs
+++ Assets/Scripts/ScriptableObject/Item/ItemPickup.cs
@@ -9,10 +9,14 @@ public class ItemPickup : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         IInventoryHolder inventoryHolder = other.GetComponent<IInventoryHolder>();
-        if (inventoryHolder != null)
+        if (inventoryHolder != null && item != null)
         {
             inventoryHolder.GetInventory().AddItem(item);
             Destroy(gameObject);
+        }
+        else
+        {
+            Debug.LogWarning($"[ItemPickup] 아이템 추가 실패 - inventoryHolder 또는 item이 null");
         }
     }
 }
