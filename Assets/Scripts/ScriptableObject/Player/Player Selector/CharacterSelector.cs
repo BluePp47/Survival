7f654655a4bb693eeca1f47c62dda6f718592ae4
sunyeji <ssuu0167@gmail.com>
2025-05-29 16:33:31 +0900

    feat :  move scene
    캐릭터 선택씬 - 게임씬 이동

--- Assets/Scripts/ScriptableObject/Player/Player Selector/CharacterSelector.cs
+++ Assets/Scripts/ScriptableObject/Player/Player Selector/CharacterSelector.cs
@@ -3,11 +3,14 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CharacterSelector : MonoBehaviour
-{
+{   
+    [Header("캐릭터 선택")]
     public GameObject[] characters;        // 캐릭터 프리팹 배열
     private int currentIndex = 0;          // 현재 선택된 캐릭터 인덱스
     private GameObject currentCharacter;   // 현재 씬에 표시된 캐릭터
     
+    
+    [Header("버튼")]
     public Button leftButton;              
     public Button rightButton;             
 
@@ -15,6 +18,7 @@ public class CharacterSelector : MonoBehaviour
     
     void Start()
     {
+        
         ShowCharacter(currentIndex); // 첫 캐릭터 보여주기
 
         leftButton.onClick.AddListener(ShowPreviousCharacter);
@@ -45,10 +49,11 @@ public class CharacterSelector : MonoBehaviour
         ShowCharacter(currentIndex);
     }
     
-    void StartGame()
+    void StartGame() //다른씬으로 이동하는 코드 !
     {
         PlayerPrefs.SetInt("SelectedCharacterIndex", currentIndex);
-        SceneManager.LoadScene("GameScene"); // 이동할 씬 이름
+        SceneManager.LoadScene("3Dsurvibe"); // 이동할 씬 이름
+        Debug.Log("게임씬 이동");
     }
     
     public float rotationSpeed = 50f; // 회전 속도 (값이 클수록 빠름)
