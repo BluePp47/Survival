99b286d3ae582f3899852895092f17db0c5b9c6d
피타쵸쵸 <djfb123@naver.com>
2025-05-29 20:30:39 +0900

    [dev] npc collider
    [dev] npc collider

--- Assets/Scripts/NPC/NpcInterAction.cs
+++ Assets/Scripts/NPC/NpcInterAction.cs
@@ -5,10 +5,24 @@ using UnityEngine.UI;
 
 public class NpcInterAction : MonoBehaviour
 {
+    public GameObject Dialog;  // 열고 닫을 UI 오브젝트
 
     private void OnTriggerEnter(Collider other)
     {
-       if()CompareTag<player>
 
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("active");
+            Dialog.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("deactive");
+            Dialog.SetActive(false);
+        }
     }
 }
