cccfae2e5f293ee8e0e99680b81ab18130834315
피타쵸쵸 <djfb123@naver.com>
2025-05-29 19:29:35 +0900

    [fix] npc script fix

---
+++ Assets/Scripts/NPC/NpcInterAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NpcInterAction : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if()CompareTag<player>
+
+    }
+}
