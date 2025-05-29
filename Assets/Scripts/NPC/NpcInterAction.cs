e5c3ab893e210ef72abcce4bccff1103eaa57827
피타쵸쵸 <djfb123@naver.com>
2025-05-29 21:01:59 +0900

    [Fix] npc close fix
    [Fix] npc  close  btn fix

--- Assets/Scripts/NPC/NpcInterAction.cs
+++ Assets/Scripts/NPC/NpcInterAction.cs
@@ -5,10 +5,25 @@ using UnityEngine.UI;
 
 public class NpcInterAction : MonoBehaviour
 {
+    public GameObject Dialog;  // 열고 닫을 UI 오브젝트
 
-    private void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
-       if()CompareTag<player>
+        
+        if (other.gameObject.CompareTag("Player")) 
+        {
+            Debug.Log("active");
+            Dialog.SetActive(true);
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+       
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("deactive");
+            Dialog.SetActive(false);
+        }
     }
 }
