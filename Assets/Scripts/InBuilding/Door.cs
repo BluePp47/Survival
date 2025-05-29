5566deddf2a786a6e2f55498eacdcfda66f78b56
song-ssi <song33@outlook.kr>
2025-05-29 12:31:49 +0900

    [Feat] InBuilding_DeskinProduction
    -제작대 UI

--- Assets/Scripts/InBuilding/Door.cs
+++ Assets/Scripts/InBuilding/Door.cs
@@ -20,7 +20,7 @@ public class SceneLoader : MonoBehaviour
         loadingUI.SetActive(true);
 
         // 살짝 대기 (로딩UI가 화면에 표시될 수 있도록)
-        yield return new WaitForSecondsRealtime(2.0f);
+        yield return new WaitForSecondsRealtime(1.5f);
 
         // 비동기 로딩 시작
         AsyncOperation async = SceneManager.LoadSceneAsync("3Dsurvibe");
@@ -34,9 +34,15 @@ public class SceneLoader : MonoBehaviour
         // 씬 로드 완료 후 원래 속도 복구 (필요한 경우)
         Time.timeScale = 1f;
     }
-
-    void Update()
+    
+    // Player가 문에 충돌시 LoadSceneWithPause() 실행
+    void OnCollisionEnter(Collision collision)
     {
-        transform.Rotate(Vector3.forward, 180f * Time.unscaledDeltaTime); // 로딩 스피너 회전
+        Debug.Log("문에닿음");
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            LoadSceneWithPause();
+        }
     }
 }
+    
